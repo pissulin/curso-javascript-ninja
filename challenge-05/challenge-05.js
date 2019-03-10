88856eb0e9c54cd690940dfd6bc1f4e5cc157b5d
pissulin <pissulin.ads@gmail.com>
2019-03-10 19:08:31 -0300

    exercicios feitos

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var arrayy = [5,4,6,8]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function myarray(arrayy){
+    return arrayy
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log (myarray)[1]
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
